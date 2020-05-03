aeb52f249a8162fb4b2b083e93c689885effa9d3
Valko <Valko05081994>
2020-05-03 19:15:50 +0300

    Add BoardView.tsx

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import {BoardView} from "./BoardView";
 
 export const Board = () => {
    return (
        <div>
-           Board!
+           <BoardView />
        </div>
    )
 }

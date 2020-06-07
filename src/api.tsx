3ce4a7d06c6e0816b082880751c5f57b70f5ed8b
ValkoBob <valko_bob@ukr.net>
2020-06-07 19:31:59 +0300

    Fix api

--- src/api.tsx
+++ src/api.tsx
@@ -1,10 +1,10 @@
 import axios from 'axios';
- /* To Start the JSON Server
-   npm install -g json-server install json-server globally
-   cd mock navigate to mock folder
-   json-server mockdata.json --port=3456 will run the api server in port 3456
- */
+import store from "./redux/store";
+
+
 export default axios.create({
-    baseURL: `https://5eb567e8de5849001638b63a.mockapi.io/api/v1/`,
-    headers: {'Content-Type': 'application/json'}
+  /*baseURL: `https://5eb567e8de5849001638b63a.mockapi.io/api/v1/`,*/
+  baseURL: `http://localhost:5000/v1/`
 });
+
+

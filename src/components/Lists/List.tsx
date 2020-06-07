cb3a0297326f7d2f110e7c6fbd8b96265b0a9e45
ValkoBob <valko_bob@ukr.net>
2020-06-07 19:29:04 +0300

    Fix Lists

--- src/components/Lists/List.tsx
+++ src/components/Lists/List.tsx
@@ -2,36 +2,39 @@ import React from "react";
 import {EditableName} from "../MultipleComponents/EditableName";
 import Cards from "../Cards";
 import ListMenu from "./ListMenu";
-import {ILists} from "../../types";
 
 interface Props {
-    lists: ILists[];
-    boardId: string;
-    editText: (id: string, newTitle: string) => void;
-    editClass: (className: string) => string;
+  lists: any;
+  boardId: number;
+  editText: (id: number, newTitle: string) => void;
+  editClass: (className: string) => string;
 }
 
 export const List = (props: Props) => {
-    const {lists, boardId, editText, editClass} = props;
-        const listsRender = lists.map((list: ILists) => {
-            if (!list.archived) {
-                return (
-                    <div key={list.id} className="board-content-list__item">
-                        <div className="board-list-header">
-                            <EditableName
-                                name={list.title}
-                                id={list.id}
-                                editText={editText}
-                                editClass={editClass} className={"board-list-name"}/>
-                            <ListMenu listId={list.id}/>
-                        </div>
-                        <Cards
-                            listId={list.id}
-                            boardId={boardId}
-                        />
-                    </div>
-                )
-            }
-        })
-    return (<>{listsRender}</>)
+  const {lists, boardId, editText, editClass} = props;
+  const listsRender = lists.map((list: any) => {
+    if (!list.archived) {
+      return (
+          <div key={list[0]} className="board-content-list__item">
+            <div className="board-list-header">
+              <EditableName
+                  name={list[1].title}
+                  id={list[0]}
+                  editText={editText}
+                  editClass={editClass} className={"board-list-name"}/>
+              <ListMenu
+                  listId={list[0]}
+                  boardId={boardId}
+              />
+            </div>
+            <Cards
+                listId={list[0]}
+                boardId={boardId}
+                cards={list[1].cards}
+            />
+          </div>
+      )
+    }
+  })
+  return (<>{listsRender}</>)
 }

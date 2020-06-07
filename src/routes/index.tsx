d2b5194c4bb51a815d1133bce5822b035d0e76c7
ValkoBob <valko_bob@ukr.net>
2020-06-07 19:29:29 +0300

    Remake routes for login and signup

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,37 @@
-import {Switch, Route} from 'react-router-dom'
-import React from "react";
-import {Login} from "../components/Login";
-import {Registration} from "../components/Registration";
-import Board from "../components/Board";
-import Boards from "../components/Boards";
+import React, {useEffect} from "react";
+import {Spinner} from "../components/MultipleComponents/Spinner";
+import {connect} from "react-redux";
+import * as actions from "../redux/actions/index";
+import {useAuth} from "../hooks/auth.hook";
+import {useRoutes} from "../hooks/routes.hook"
+import {AuthContext} from '../context/AuthContext'
 
 
-const Routes = () => (
-    <Switch>
-        <Route exact path="/" component={Boards}/>
-        <Route path="/login" component={Login}/>
-        <Route path="/registration" component={Registration}/>
-        <Route exact path="/boards" component={Boards}/>
-        <Route path="/b/:id_board/:name_board" component={Board}/>
-    </Switch>
-)
+const Routes = (props: any) => {
+  const {token, login, logout} = useAuth()
+  const {isAuthenticated, loading} = props;
+  const routes = useRoutes(isAuthenticated)
+  useEffect(()=> {
+    if(token) {
+      props.userInLocal(token)
+    }
+  }, [token])
+  if (loading) {
+    return <Spinner/>
+  }
+  return (
+      <AuthContext.Provider value={{
+        // @ts-ignore
+        token, login, logout
+      }}>
+        {routes}
+      </AuthContext.Provider>)
+}
 
-export default Routes
+const mapStateToProps = (state: any) => {
+  const {isAuthenticated} = state.users
+  const {loading} = state.dataRequest
+  return {isAuthenticated, loading}
+}
+
+export default connect(mapStateToProps, actions)(Routes)

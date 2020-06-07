88aa8a5fc55a98c93b68a56760595d604e0dcfe1
ValkoBob <valko_bob@ukr.net>
2020-06-07 19:31:50 +0300

    Fix App

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router} from 'react-router-dom'
+import {Provider} from 'react-redux';
+import {BrowserRouter as Router} from 'react-router-dom'
 import store from './redux/store';
 import './App.scss';
 import Routes from "./routes";
-import {NavBar} from "./components/NavBar";
 
 const App: React.FC = () => {
   return (
       <Provider store={store}>
-          <div className="app-container">
-              <Router>
-                  <NavBar />
-                  <Routes />
-              </Router>
-          </div>
+        <Router>
+          <Routes/>
+        </Router>
       </Provider>
   );
 };
